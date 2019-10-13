2431e9a12135d0d86fd6e6dcdde6b0d7c41136b0
Paul Wolf <49984289+flowluap@users.noreply.github.com>
2019-10-13 11:01:20 +0200

    rewritten for python3 | beep after card validation
    Working on a project and found this repo very useful; thanks!

--- myfare.py
+++ myfare.py
@@ -1,33 +1,32 @@
+#optimized for pyton3
+
 import serial
-from struct import pack, unpack
+import binascii
 
 prot = {
     'enquiry_module': '\x03\x12\x00\x15',
     'enquiry_module_return': '\x02\x12\x14',
     'active_buzzer': '\x02\x13\x15',
-    'enquiry_card': pack('BBBB', 03, 02, 00, 05),
+    'enquiry_card': '\x03\x02\x00\x05',
     'enquiry_cards_return': '\x03\x02\x01\x06', # got valid card
     'enquiry_no_card_found': '\x02\x01\x03', # no card reachable or invalid
     'enquiry_all_cards': '\x03\x02\x01\x05',
     'anticollision' : '\x02\x03\x05\x00',
     'select_card' : '\x02\x04\x06',
 }
-
 ser = serial.Serial('/dev/ttyUSB0',9600,timeout=1)
-ser.write(prot['enquiry_card'])
-resp =  ser.read(4)
 
-if resp == prot['enquiry_no_card_found']:
-    print "no valid card reachable"
-elif resp == prot['enquiry_cards_return']:
-    ser.write(prot['active_buzzer'])
-    resp = ser.read(3)
-    if resp == prot['active_buzzer']:
-        print "found card, sending anticollision"
-        ser.write(prot['anticollision'])
+while True:
+    ser.write(prot['enquiry_card'].encode())
+    resp =  ser.read(4)
+    if resp == prot['enquiry_cards_return'].encode():
+        resp = ser.read(3)
+        ser.write(prot['anticollision'].encode())
         resp = ser.read(7)
-        header = resp.encode('hex')[:4]
-        if header == "0603":
-            print "Card Serial:" + resp[-5:].encode('hex')
+        header = binascii.b2a_hex(resp)[:4].decode("utf-8")
+
+        if header == "0603" and binascii.b2a_hex(resp)[-5:].decode("utf-8"):
+            ser.write(prot['active_buzzer'].encode())
+            print(binascii.b2a_hex(resp).decode("utf-8"))
 
 ser.close()
